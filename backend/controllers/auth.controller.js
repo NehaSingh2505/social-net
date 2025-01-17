778516b5e2b15ffe64cbc8f99f4321ef9af3d4f4
lakshya mittal <lakshyamittal2405@gmail.com>
2025-01-17 20:43:14 +0530

    updated

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,22 @@
+import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+	try {
+		const { fullName, username, email, password } = req.body;
+
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		if (!emailRegex.test(email)) {
+			return res.status(400).json({ error: "Invalid email format" });
+		}
+
+		const existingUser = await User.findOne({ username });
+		if (existingUser) {
+			return res.status(400).json({ error: "Username is already taken" });
+		}
+
+		const existingEmail = await User.findOne({ email });
+		if (existingEmail) {
+			return res.status(400).json({ error: "Email is already taken" });
+		}
