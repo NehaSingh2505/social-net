523d70a3182242c8ff203ccd3d5dfac033dbc0ea
lakshya mittal <lakshyamittal2405@gmail.com>
2025-01-17 21:31:27 +0530

    updates

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -20,3 +20,37 @@ export const signup = async (req, res) => {
 		if (existingEmail) {
 			return res.status(400).json({ error: "Email is already taken" });
 		}
+
+        if (password.length < 6) {
+			return res.status(400).json({ error: "Password must be at least 6 characters long" });
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		const newUser = new User({
+			fullName,username,email,password: hashedPassword,
+		});
+
+		if (newUser) {
+			generateTokenAndSetCookie(newUser._id, res);
+			await newUser.save();
+
+			res.status(201).json({
+				_id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+				email: newUser.email,
+				followers: newUser.followers,
+				following: newUser.following,
+				profileImg: newUser.profileImg,
+				coverImg: newUser.coverImg,
+			});
+		} else {
+			res.status(400).json({ error: "Invalid user data" });
+		}
+	} catch (error) {
+		console.log("Error in signup controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
