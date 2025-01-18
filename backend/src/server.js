473c47dc0cd36cc1bd4e792ca8e163013aae286f
lakshya mittal <lakshyamittal2405@gmail.com>
2025-01-19 00:51:08 +0530

    logout code updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./auth');
-
+const { connect } = require('mongoose');
+import authRoutes from "./routes/auth.route.js";
+import  connectMongoDB from"./db/connectMongoDB.js";
 dotenv.config();
 const app = express();
 
@@ -16,4 +18,5 @@ app.use('/api/auth', authRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>{ console.log(`Server running on port ${PORT}`);
+connectMongoDB();});
